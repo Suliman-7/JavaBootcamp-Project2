de1bfb9efee60e6acf1e0f313da68ef07dce9daf
Suliman Alodaib <107861497+Suliman-7@users.noreply.github.com>
2024-07-22 14:44:49 +0300

    Add files via upload

---
+++ NonSubscribers.java
@@ -0,0 +1,62 @@
+
+public class NonSubscribers extends Passenger {
+
+    private boolean discountCoupon ;
+
+
+    public NonSubscribers(String id, String name, Car reservedCar, int tripCost,boolean discountCoupon) {
+        super(id, name, reservedCar, tripCost);
+        this.discountCoupon = discountCoupon;
+
+    }
+
+    public boolean isDiscountCoupon() {
+        return discountCoupon;
+    }
+
+    public void setDiscountCoupon(boolean discountCoupon) {
+        this.discountCoupon = discountCoupon;
+    }
+
+
+    @Override
+    public void reserveCar(Car car) throws ArithmeticException  {
+
+        double tripCost ;
+
+        try {
+
+
+            if (car.getMaxPerTrip()==0) {
+                throw new ArithmeticException ("Unfortunately , Maximum Capacity per trip is Zero") ;
+            }
+
+
+            if (isDiscountCoupon()) {
+                super.setTripCost(car.getFiexedRoute().getTripPrice() - (car.getFiexedRoute().getTripPrice() * 0.1)) ;
+            }
+            else {
+                super.setTripCost(car.getFiexedRoute().getTripPrice()) ;
+            }
+            car.setMaxPerTrip(car.getMaxPerTrip() - 1);
+            super.setReservedCar(car);
+
+
+
+
+        }catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+
+
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Passenger non subscriber " + super.getName());
+        System.out.println("ID : " + super.getId() + " Reserved Car code : " + super.getReservedCar().getCode() + " Trip Cost : " + super.getTripCost() );
+    }
+}
